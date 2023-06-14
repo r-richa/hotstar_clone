bb2a732794b9cae07fd12dbd7935bd140fa3129e
r-richa <richarani2025@gmail.com>
2023-06-14 21:15:08 +0530

    second commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './LoginPage.css';
+
+
+
+const LoginPage = () => {
+    return (
+        <div className="login">
+            <div className="title">
+                <h1>Login</h1>
+            </div>
+            <div className="form">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginPage;
